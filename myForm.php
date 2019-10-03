31b9a7ab1a9645090e2e73b6c4651ed6846e06bf
Jorge Espinoza <je88@afsconnect1.njit.edu>
2019-10-03 12:00:56 -0400

    myForm Stuff

---
+++ myForm.php
@@ -0,0 +1,25 @@
+<?
+init_set('display_errors',1);
+init_set('display_startup_errprs',1);
+error_reposting(E_ALL);
+function getName(){
+	if(isset($_GET['name'])){
+		echo "<p>Hello, " . $_GET['name'] . "</p>";
+	}
+}
+?>
+<html>
+<head></head>
+<body><?php getName<>;?>
+<form mode="GET" action="#">
+<input name="name" type="text" placeholder="Enter your name"/>
+<input type="submit" value = "Try it"/>
+</form>
+</body>
+</html>
+
+<?
+if(isset($_GET){
+	echo "<br><pre>" . var_exports($_GET, true) . "</pre><br>";
+}
+?>
