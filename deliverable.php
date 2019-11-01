d1bc1a456157cefa58273290c537a6c871c98658
Jorge Espinoza <je88@afsconnect1.njit.edu>
2019-11-01 11:18:27 -0400

    Deliverable Progress

--- deliverable.php
+++ deliverable.php
@@ -1,42 +1,39 @@
-<form>
-<input type="text" name="username"/>
-<input type="password" name="password"/>
-<input type="submit" value="Let me innnnnn??">
-</form>
-
 <?php
 
 require("config.php"); 
+
+ini_set('display_errors',1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
 
 if(isset($_POST['usename']) && isset($_POST['pin'])){
         $username = $_POST['username'];                                                                                                       $pin = $_POST['pin'];
         $pin = $_POST['pin'];
-
-	$db = new PDO($conn_string, $username, $password);
+	
+	try{
+	$db = new PDO($conn_string, $configUser, $configPass);
 	echo "Connected";
-	//create table
+	}
+	catch(Exception $e){
+		echo"Connection could not be established: " . $e->getMessage();
+	}
+	
+	try{
 	$query = "create table if not exists `SignUp`(
-		`id` int auto_increment not null,
 		`username` varchar(30) not null unique,
 		`pin` int default 0,
 		PRIMARY KEY (`id`)
 		) CHARACTER SET utf8 COLLATE utf8_general_ci";
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
-	$stmt = $db->prepare($query);
-	print_r($stmt->errorInfo());
-	$results = $stmt->execute();
-	echo "<br>" . ($r>0?"Created table or already exists":"Failed to create table") . "<br>";
-	unset($results);
-	
-	
-	$insert_query = "INSERT INTO `SignUp`( `username`, `pin`) 
-		VALUES (:username, :pin)";
-	$stmt = $db->prepare($insert_query);
-	$newUser = "Temp";
-	$newPin = 1234;
-	$r = $stmt->execute(array(":username"=> $newUser, ":pin"=>$newPin));
-	unset($r);
+	$insert_query = "INSERT INTO `SignUp`( `username`, `pin`)
+        	VALUES (:username, :pin)";
+        $stmt = $db->prepare($insert_query);
+        $newUser = "Temp";
+        $newPin = 1234;
+        $r = $stmt->execute(array(":username"=> $newUser, ":pin"=>$newPin));
+        unset($r);
 
 	print_r($stmt->errorInfo());
 	
@@ -51,10 +48,37 @@ if(isset($_POST['usename']) && isset($_POST['pin'])){
 	
 	$response = "Welcome Back $username";
         echo/return $response;	
-}
-catch(Exception $e){
-	echo $e->getMessage();
-	exit("Something's wrong here");
-}
+	}
 
+	catch(Exception $e){
+		echo $e-getMessage();
+		echo "Somethings wrong here";
+}
 ?>
+
+<div>
+	<h2>Username:</h2>
+	<p>
+	<?php
+	if(isset($results['username'])){
+		echo $username;
+	}
+	else{
+		echo "User not found";
+	}
+	?>
+	</p>
+
+	<h2>Password:</h2>
+	<?php
+	if(isset($results['pin'])){
+		echo $pin;
+	}
+	else{
+		echo "Password not found";
+	}
+	?>
+	<p>
+
+	</p>
+</div>
