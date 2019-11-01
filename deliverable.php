24fadf2d8dbb69fb1229a6e56be68cd58e210dbd
Jorge Espinoza <je88@afsconnect1.njit.edu>
2019-11-01 12:23:51 -0400

    ProgressStill

--- deliverable.php
+++ deliverable.php
@@ -1,10 +1,10 @@
 <?php
 
-require("config.php"); 
-
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+
+require('config.php');
 
 $conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
 
@@ -15,28 +15,27 @@ if(isset($_POST['usename']) && isset($_POST['pin'])){
 	try{
 		$db = new PDO($conn_string, $configUser, $configPass);
 		echo "Connected";
-	}
-	catch(Exception $e){
-		echo"Connection could not be established: " . $e->getMessage();
-	}
-	
-	try{
+
 	$query = "create table if not exists `SignUp`(
 		`username` varchar(30) not null unique,
 		`pin` int default 0,
 		PRIMARY KEY (`id`)
 		) CHARACTER SET utf8 COLLATE utf8_general_ci";
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+	$stmt = $db->prepare($query);
+	print_r($stmt->errorInfo());
+	$r = $stmt->execute();
+	echo "<br>" . ($r>0?"Created table or already exists":"Failed to create table") . "<br>";
+	unset($r);
+
 	$insert_query = "INSERT INTO `SignUp`( `username`, `pin`)
         	VALUES (:username, :pin)";
         $stmt = $db->prepare($insert_query);
         $newUser = "Temp";
         $newPin = 1234;
         $r = $stmt->execute(array(":username"=> $newUser, ":pin"=>$newPin));
-        unset($r);
 
 	print_r($stmt->errorInfo());
-	
 	
 	echo "<br>" . ($r>0?"Insert successful":"Insert failed") . "<br>";
 	
@@ -44,6 +43,7 @@ if(isset($_POST['usename']) && isset($_POST['pin'])){
 	$stmt = $db->prepare($select_query);
 	$r = $stmt->execute(array(":username"=> "Temp"));
 	$results = $stmt->fetch(PDO::FETCH_ASSOC);
+	
 	echo "<pre>" . var_export($results, true) . "</pre>"; 
 	
 	$response = "Welcome Back $username";
