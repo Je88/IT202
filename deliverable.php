a3b90a0ac2763366cd610cc506249da0cac735b2
Jorge Espinoza <je88@afsconnect1.njit.edu>
2019-11-05 12:39:33 -0500

    Progressing Stillllll

--- deliverable.php
+++ deliverable.php
@@ -16,12 +16,11 @@ error_reporting(E_ALL);
 	<label for="password">Password:</label>
     <input type="password" name="password"/>
 
-    <input type="submit" Value="Lets GOOOOOOOO"/>
+    <input type="submit" Value="Enter"/>
     </form>
 </body>
 </html>
 <?php
-$response = '';
 	if(isset($_POST['username']) && isset($_POST['pin'])){
 		$user = $_POST['username'];
 		$pin = $_POST['pin'];
@@ -30,12 +29,7 @@ $response = '';
 			require('config.php')                                                                                          	
 			$conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4"; 
 			$db = new PDO($conn_string, $username, $password);
-			echo "Connected";
-		$query = "create table if not exists `SignUp`(
-			`username` varchar(30) not null unique,
-			`pin` int default 0,
-			PRIMARY KEY (`id`)
-			) CHARACTER SET utf8 COLLATE utf8_general_ci";
+
 			$stmt = $db->prepare("INSERT into `SignUp` (`username`, `password`) VALUES(:username, :password)");
 			print_r($stmt->errorInfo());
 			$results = $stmt->execute();
@@ -43,24 +37,17 @@ $response = '';
 			unset($results);
 			//$newUser = "Temp";
 			//$newPin = 1234;
-			//$results = $stmt->execute(array(":username"=> $newUser, ":pin"=>$newPin));
-	
-			$stmt->bindValue(':username',$username);
-			$stmt->excute();
-			print_r($stmt->errorInfo());
+			//$results = $stmt->execute(array(":username"=> $newUser, ":pin"=>$newPin));	
 	
 			echo "<br>" . ($r>0?"Insert successful":"Insert failed") . "<br>";
-			$results = $stmt->execute(array(":username"=> $username));
 			$results = $stmt->fetch(PDO::FETCH_ASSOC);
 	
 			echo "<pre>" . var_export($results, true) . "</pre>"; 
 	
-			$response = "Welcome Back " . $username;
-	}
+			echo "Welcome Back " . $username;
+		}
 	catch(Exception $e){
-		$response = "Something's wrong here: " . $e;
+		echo "Something's wrong here: " . $e;
 	}
-	return $response;
-	}
-echo $response;
+}
 ?>
