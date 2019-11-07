dc3ba8f338fef68f1790fda037741caa1671257d
Jorge Espinoza <je88@afsconnect1.njit.edu>
2019-11-07 08:59:19 -0500

    IDK

--- regideliverable.php
+++ regideliverable.php
@@ -8,7 +8,7 @@ error_reporting(E_ALL);
 <title> Login </title>
 <head>
 <body>
-	<form method="POST"/>
+	<form method="POST">
 		<input type="text" name="username"/>
 		<input type="password" name="password"/>
 		<input type="submit" value="ENTER"/>
@@ -16,27 +16,22 @@ error_reporting(E_ALL);
 </body>
 </html>
 <?php
-	if(isset($_POST['username']) 
-		&& isset($_POST['password']){
+	echo "hello";
+	if(isset($_POST['username']) && (isset($_POST['password']){
 			
 		$user = $_POST['username'];
-		$pin = $_POST['password'];
-				//exit();
-		}
-		//do further validation?
-		try{
-			require("config.php");
-			$conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
-			$db = new PDO($conn_string, $username, $pin);
-			$stmt = $db->prepare("INSERT into `SignUp` (`username`, `password`) VALUES(:username, :password)");
-			$result = $stmt->execute(
-				array(":username"=>$user
-				)
-			);
-			print_r($stmt->errorInfo());
-			
-			echo var_export($result, true);
-		}
+		$password = $_POST['password'];
+		echo "hello";		
+//		try{
+//			require("config.php");
+//			$conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
+//			$db = new PDO($conn_string, $username, $password);
+//			$stmt = $db->prepare("INSERT into `SignUp` (`username`, `password`) VALUES(:username, :password)");
+//			$result = $stmt->execute(array(":username"=>$user));
+//			print_r($stmt->errorInfo());
+//			
+//			echo var_export($result, true);
+//		}
 		catch(Exception $e){
 			echo $e->getMessage();
 		}
