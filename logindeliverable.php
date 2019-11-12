8905602eed2fb44672231ada36c82cf5523ef127
Jorge Espinoza <je88@afsconnect1.njit.edu>
2019-11-12 12:44:32 -0500

    Renamed

--- logindeliverable.php
+++ logindeliverable.php
@@ -6,24 +6,7 @@ error_reporting(E_ALL);
 
 <html>
 <head>
-<title> Register </title>
-
-<script>
-$(document).ready(function(){
-	$('#register_form').submit(function(event){
-		if(this.password.value.length == 0 || this.check.value.length == 0){
-			alert("Please enter a password and confirm it");
-			return false;
-		}
-		let isOk = this.password.value == this.check.value;
-		if(!isOk){
-			alert("Password and Confirm password don't match");
-		}
-		return isOk;
-	});
-});
-</script>
-
+<title> Login </title>
 </head>
 <body>
 	<form method="POST"/>
@@ -32,10 +15,7 @@ $(document).ready(function(){
 		
 		<label for="password">Password </label>
 		<input type="password" name="password"/>
-
-		<label for="password">Pass Check </label>
-                <input type="password" name="check"/>
-
+	
 		<input type="submit" value="WELCOME?"/>
 	</form>
 </body>
@@ -46,23 +26,16 @@ $(document).ready(function(){
 			
 		$user = $_POST['username'];
 		$pass = $_POST['password'];
-		$check = $_POS['check']
 		
-		if($pass != $confirm){
-			echo "Passwords don't match";
-			exit();
-		}
-
 		try{
-			$hash = password_hash($pass, PASSWORD_BCRYPT);
+			//$hash = password_hash($pass, PASSWORD_BCRYPT);
 			require("config.php");
 			$conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
 			$db = new PDO($conn_string, $username, $password);
 			$stmt = $db->prepare("INSERT into `SignUp` (`username`, `pin`) VALUES(:username, :pin)");
-			$result = $stmt->execute(array(":username"=>$user, ":pin"=>$hash));
+			$result = $stmt->execute(array(":username"=>$user, ":pin"=>$pass));
 			echo "<p>";
 			print_r($stmt->errorInfo());
-
 			echo var_export($result,true);
 			if((var_export($result,true)) == true){
 			echo "<p> Welcome " . $user;
@@ -70,7 +43,6 @@ $(document).ready(function(){
 			else{
 			echo "Username Already Exists. Did you forget your password?";
 			}
-
 		}
 		catch(Exception $e){
 			echo "Something's wrong here: " . $e->getMessage();
