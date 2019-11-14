05b54bd7c17c47b5f5ec52155d77571b4d6126eb
Jorge Espinoza <je88@afsconnect1.njit.edu>
2019-11-14 12:50:22 -0500

    StillNOtWOrking

--- logindeliverable.php
+++ logindeliverable.php
@@ -21,21 +21,18 @@ error_reporting(E_ALL);
 </body>
 </html>
 <?php
-	if(isset($_POST['username']) 
-		&& isset($_POST['password'])){
+	if(isset($_POST['username']) && isset($_POST['password'])){
 			
 		$user = $_POST['username'];
 		$pass = $_POST['password'];
-		
 		try{
 			require("config.php");
 			$conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
 			$db = new PDO($conn_string, $username, $password);
-			$stmt = $db->prepare("select username, pin from `SignUp` 
-			where username = :username LIMIT 1");
+			$stmt = $db->prepare("select username, pin from `SignUp` where username = :username LIMIT 1");
 			$stmt->execute(array(":username"=>$user));
 			$results = $stmt->fetch(PDO::FETCH_ASSOC);
-			echo "<p>";
+			
 			if(($results && count($results) > 0){
 				if(password_verify($pass, $results['password'])){
 					echo "Welcome, " . $results["username"];	
@@ -44,7 +41,7 @@ error_reporting(E_ALL);
 					$_SESSION['user'] = $user;
 					echo var_export($user, true);
 					echo var_export($_SESSION, true);
-					header("Location: landingpage.php")
+					//header("Location: landingpage.php");
 				}	
 				else{
 					echo "Invalid Password";
