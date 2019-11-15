efd19d05c15d32e24655be3046e47e78c70c6766
Jorge Espinoza <je88@afsconnect1.njit.edu>
2019-11-15 11:10:47 -0500

    ProjStuff

---
+++ landingpage.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<style>
+.nav{padding:1%;}
+</style>
+</head>
+<body>
+<?php
+include_once("partials/header.php");
+?>
+
+<section>Welcome, <?php get_username();?>.</section>
+<section>
+<header>Items Due Soon</header>
+<p>Item one</p>
+<p>Item two</p>
+<p>Item three</p>
+<p>...</p>
+</section>
+</body>
+</html>
--- logindeliverable.php
+++ logindeliverable.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 ini_set('display_errors',1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
@@ -9,50 +10,53 @@ error_reporting(E_ALL);
 <title> Login </title>
 </head>
 <body>
-	<form id="register_form" method="POST"/>
-		<label for="username">Username </lable>
-		<input type="text" name="username"/>
-		
-		<label for="password">Password </label>
-		<input type="password" name="password"/>
-	
-		<input type="submit" value="WELCOME For Real"/>
-	</form>
+        <form method="POST"/>
+                <label for="username">Username </label>
+                <input type="text" name="username"/>
+
+                <label for="password">Password </label>
+                <input type="password" name="password"/>
+
+                <input type="submit" value="WELCOME For Real"/>
+        </form>
 </body>
 </html>
 <?php
-	if(isset($_POST['username']) && isset($_POST['password'])){
-			
-		$user = $_POST['username'];
-		$pass = $_POST['password'];
-		try{
-			require("config.php");
-			$conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
-			$db = new PDO($conn_string, $username, $password);
-			$stmt = $db->prepare("select username, pin from `SignUp` where username = :username LIMIT 1");
-			$stmt->execute(array(":username"=>$user));
-			$results = $stmt->fetch(PDO::FETCH_ASSOC);
-			
-			if(($results && count($results) > 0){
-				if(password_verify($pass, $results['password'])){
-					echo "Welcome, " . $results["username"];	
-					echo "[" . $results["id"] . "]";
-					$user = array("id" => $results['id'], "name" => $results['username']);
-					$_SESSION['user'] = $user;
-					echo var_export($user, true);
-					echo var_export($_SESSION, true);
-					//header("Location: landingpage.php");
-				}	
-				else{
-					echo "Invalid Password";
-				}
-			}
-			else{
-				echo "Invalid Username";
-			}
-		}
-		catch(Exception $e){
-			echo  $e->getMessage();
-		}
-	}
+        if(isset($_POST['username']) && isset($_POST['password'])){
+                $user = $_POST['username'];
+                $pass = $_POST['password'];
+                //do further validation?
+                try{
+                        require("config.php");
+                        //$username, $password, $host, $database
+                        $conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
+                        $db = new PDO($conn_string, $username, $password);
+                        $stmt = $db->prepare("select username, pin from `SignUp` where username = :username LIMIT 1");
+                        $stmt->execute(array(":username"=>$user));
+                        //print_r($stmt->errorInfo());
+                        $results = $stmt->fetch(PDO::FETCH_ASSOC);
+                        //echo var_export($results, true);
+                        if($results && count($results) > 0){
+                                //$hash = password_hash($pass, PASSWORD_BCRYPT);
+                                if(password_verify($pass, $results['password'])){
+                                        echo "Welcome, " . $results["username"];
+                                        $user = array("name" => $results['username']);
+                                        $_SESSION['user'] = $user;
+                                        echo var_export($user, true);
+                                        echo var_export($_SESSION, true);
+                                        header("Location: landingpage.php");
+                                        
+                                }
+                                else{
+                                        echo "Invalid password";
+                                }
+                        }
+                        else{
+                                        echo "Invalid username";
+                        }
+                }
+                catch(Exception $e){
+                        echo $e->getMessage();
+                }
+        }
 ?>
