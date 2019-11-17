00d9f812a60b872c24b8778f936b42d8caa5f033
Jorge Espinoza <je88@afsconnect1.njit.edu>
2019-11-16 19:44:50 -0500

    Still Workingggg

--- gamepage.php
+++ gamepage.php
@@ -11,7 +11,7 @@ session_start();
 <section>Welcome <?php echo $_SESSION["username"]; ?></section>
 </head>
 <body>
-        <form action="homepage.php" style="display: inline-grid">
+       <form action="homepage.php" style="display: inline-grid">
         <input type="submit" value="Single Player"/>
         </form>
 	<br>
@@ -30,6 +30,16 @@ session_start();
 	<form action="homepage.php" style="display: inline-grid">
         <input type="submit" value="Accounts & Scores"/>
         </form>
+	<ul id="menu">
+		<li><a href="home.php">Single Player</a></li>
+		<li><a href="shop.php">Multi-Player</a></li>
+		<li><a href="faq.php">Scoreboard</a></li>
+		<li><a href="logout.php">Logout</a></li>
+		<li><a href="registration.php">Register</a></li>
+		<?php if($_SESSION['username'] = "admin"): ?>
+			<li><a href="Admin.php">Accounts & Scores</a></li>
+		<?php endif; ?>
+	</ul>
 
 </body>
 </html>
