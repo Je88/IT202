8a2f70e3495a35ebf42b5b9747e73b1ac53234ce
Jorge Espinoza <je88@afsconnect1.njit.edu>
2019-11-18 17:36:56 -0500

    Working on Look and Functionality

---
+++ AboutMe.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+.center {
+  text-align: center;
+  border: 3px solid green;
+}
+</style>
+</head>
+<body>
+
+<div class="center">
+<h2>About The Game</h2>  
+<p>This game features an immortal creature that hunts you all 
+the while you also race against the clock to collect the scattered gems.</p>
+</div>
+
+</body>
+</html>
--- landingpage.php
+++ landingpage.php
@@ -1,22 +1,40 @@
 <?php
 session_start();
 
-//echo $_SESSION["username"];
 ?>
 
 <!DOCTYPE html>
+<html>
 <head>
 <style>
+.button{
+  background-color: #4CAF50;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+}
 .nav{padding:1%;}
 </style>
-	//if(register = true){
-		<section>Continue to login <?php echo $_SESSION["username"]; ?></section>
-	
-	//}
+
 </head>
 <body>
-	<form action="gamepage.php" style="display: inline-grid">
-	<input type="submit" value="Login"/>
-	</form>
+	<h2>Continue to login <?php echo $_SESSION["username"]; ?></h2>
+
+	<a href="regideliverable.php" class ="button">Register</a>
+	<br>
+	<?php if(empty($_SESSION['username'])): ?>
+		<button>Not Logged</button>
+		<br>	
+	<?php else: ?>	
+		<a href="gamepage.php" class ="button">Login</a>
+		<br>
+	<?php endif; ?>
+	<a href="AboutMe.php" class ="button">About</a>
 </body>
 </html>
