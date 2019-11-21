823b64d913b381a9d811adcfdfd4b75fa1b5edc3
Jorge Espinoza <je88@afsconnect1.njit.edu>
2019-11-21 12:53:26 -0500

    Works

--- regideliverable.php
+++ regideliverable.php
@@ -1,5 +1,4 @@
 <?php
-session_start();
 ini_set('display_errors',1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
@@ -13,17 +12,15 @@ error_reporting(E_ALL);
 	<form method="POST"/>
 		<label for="username">Username </lable>
 		<input type="text" name="username"/>
-		
+		<br><br>
 		<label for="password">Password </label>
 		<input type="password" name="password"/>
-
+		
 		<label for="password">Pass Check </label>
                 <input type="password" name="confirm"/>
+		<br><br>
+		<input type="submit" value="Register"/>
 		<br>
-		<input type="submit" value="WELCOME?"/>
-		<br>
-		
-		
 	</form>
 </body>
 </html>
@@ -36,25 +33,24 @@ error_reporting(E_ALL);
 		if($pass != $confirm){
                         echo "Passwords don't match";
                         exit();
-                }
-                else{
-                echo "<br>";
-		echo "Congrats on Registering " . $_POST["username"];
-		
-                }
+                }	
 
 		try{
 			$hash = password_hash($pass, PASSWORD_BCRYPT);
 			require("config.php");
 			$conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
 			$db = new PDO($conn_string, $username, $password);
-			$stmt = $db->prepare("INSERT into `SignUp` (`username`, `pin`) VALUES(:username, :pin)");
+			$stmt = $db->prepare("INSERT into `SignUp` (`Username`, `Pin`) VALUES(:username, :pin)");
 			$stmt->execute(array(":username"=>$user, ":pin"=>$hash));
 			$results = $stmt->fetch(PDO::FETCH_ASSOC);
+			echo $stmt->errorInfo();
+			echo var_export($results,true);
 			echo "<p>";
-			//print_r($stmt->errorInfo());
-			//echo var_export($results,true);
-
+		
+			if($pass = $confirm){
+                                header("Location: landingpage.php");
+                        }
+			
 		}
 		catch(Exception $e){
 			echo "Something's wrong here: " . $e->getMessage();
