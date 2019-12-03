cff502b47fd1c1d5d2ddac9833d1a8175bdb3d95
Jorge Espinoza <je88@afsconnect1.njit.edu>
2019-12-02 19:35:51 -0500

    Tried Ajax not working

--- scoreUpdate.php
+++ scoreUpdate.php
@@ -1,18 +1,20 @@
 <?php
 session_start();
 include 'singleplayer.html';
+
 require('config.php');
 $conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
 try{
-	$user = $_SESSION['username'];
-	$score = $_SESSION['score'];
+	$user = $_POST['username'];
+	$score = $_POST['score'];
 	$db = new PDO($conn_string, $username, $password);
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $scoreUpdate = "Update `SignUp` SET `Score`= :score WHERE `Username` = :user";
-	$stmt = $db->prepare($scoreUpdate);
-	//$stmt->execute([':score'=>$score]);
+        $update = "Update `SignUp` SET `Score` = '$score' WHERE `Username` = '$user'";
+	$stmt = $db->prepare($update);
+	//$stmt->execute([':user'=>$user]);
 }
 catch(Exception $e){
 		echo "DB Error: " . $e;
 	}
+//header("Location: gamepage.php");
 ?>
