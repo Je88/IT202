63dbd48e0c0e40edf8bff4ad827ef3435b4e22a2
Jorge Espinoza <je88@afsconnect1.njit.edu>
2019-12-03 12:49:05 -0500

    update

--- scoreUpdate.php
+++ scoreUpdate.php
@@ -1,20 +1,26 @@
 <?php
 session_start();
-include 'singleplayer.html';
 
 require('config.php');
 $conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
 try{
-	$user = $_POST['username'];
+	$user = $_SESSION['username'];
 	$score = $_POST['score'];
 	$db = new PDO($conn_string, $username, $password);
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $update = "Update `SignUp` SET `Score` = '$score' WHERE `Username` = '$user'";
+        $update = "Update `SignUp` SET `Score` = ':score' WHERE `Username` = ':user'";
 	$stmt = $db->prepare($update);
-	$stmt->execute([":username"=>$user, ":score"=>$score]);
+	$stmt->bindParam(':username', $_SESSION['username']);
+	$stmt->bindParam(':score', $_POST['score']);
+	//$result = $stmt->execute(array($_SESSION["username"], $_POST["score"]));
+	//$result = "Updated Successfully";
+	$stmt->execute([":user"=>$user, ":score"=>$score]);
+	
 }
 catch(Exception $e){
 		echo "DB Error: " . $e;
+		//$return = "Could not update Error: " . $e->getMessage();
 	}
-//header("Location: gamepage.php");
+
+//echo json_encode($return);
 ?>
