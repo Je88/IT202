3f5f06a6d0e4f85eface6b383e52a0dec4529c3e
Jorge Espinoza <je88@afsconnect1.njit.edu>
2019-12-07 16:40:08 -0500

    Score Table NOSHOW

---
+++ Top7Chart.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+require('config.php');
+$conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
+	try{
+		
+		$db = new PDO($conn_string, $username, $password);
+		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	        $seven = "SELECT `Username`, `Score` FROM `SignUp` ORDER BY `Score` DESC LIMIT 7";
+		$stmt = $db->prepare($seven);
+		$r = $stmt->execute();
+		return $stmt->fetchAll();
+		
+		//echo var_export($_SESSION);
+		//echo var_export($stmt->errorInfo());
+		//$stmt->execute(WHAT AM I EVEN EXECUTING?);
+	}
+	catch(Exception $e){
+		echo "DB Error: " . $e;
+	}
+<table>
+<?php foreach($r as $index=> $row): ?>
+<tr><td><?php echo $row["columnOne"];?> </td>...</tr>
+<?php endforeach; ?>
+</table>
+?>
--- scoreUpdate.php
+++ scoreUpdate.php
@@ -8,7 +8,7 @@ $conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
 		$score = $_POST['score'];
 		$db = new PDO($conn_string, $username, $password);
 		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	        $update = "UPDATE `SignUp` SET `Score` = :score WHERE `Username` = :user AND `Score` > :score";
+	        $update = "UPDATE `SignUp` SET `Score` = :score WHERE `Username` = :user AND `Score` < :score";
 		$stmt = $db->prepare($update);
 		$stmt->bindValue(':user', $_SESSION['username']);
 		$stmt->bindValue(':score', $_POST['score']);
